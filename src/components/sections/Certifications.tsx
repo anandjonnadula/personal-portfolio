157d3e24beb370958c377cbc0c0e4ae4172497e3
Anand Jonnadula <anandjonn8@gmail.com>
2026-08-17 12:04:50 +0530

    modified:   src/components/sections/Certifications.tsx

--- src/components/sections/Certifications.tsx
+++ src/components/sections/Certifications.tsx
@@ -21,7 +21,7 @@ export default function Certifications() {
                 <div className="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-between">
                   <div>
                     <div className="flex flex-wrap items-center gap-3">
-                      <h3 className="font-display text-lg font-semibold text-ink accent">
+                      <h3 className="font-display text-lg font-semibold text-ink font-color">
                         {certification.index}&nbsp;&nbsp;&nbsp;&nbsp;
                         {certification.label}
                       </h3>
